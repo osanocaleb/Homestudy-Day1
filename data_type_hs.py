bd9d4c266606302319502a1355541fa9fa946b40
osanocaleb <caleb.osano.co@gmail.com>
2016-10-31 17:35:11 +0300

    Updates to the code

--- data_type_hs.py
+++ data_type_hs.py
@@ -1,21 +1,21 @@
 def data_type(s):
-
   if isinstance(s,str):
     return len(s)
-  elif isinstance(s,type(none)):
-      return"no value"
+  
   elif isinstance(s,bool):
             return (s)
+  elif s is None:
+    return 'no value'
   elif isinstance(s, int):
             if s<100:
-              return "less than 100"
+              return 'less than 100'
             elif s>100:
-              return"more than 100"
+              return'more than 100'
             else:
-              return"equal to 100"
+              return'equal to 100'
 
   elif isinstance(s,list):
-    if len(s)>=[3]:
-        return s [2]
-    else:
+    if len(s)>=3:
         return s[2]
+    else:
+        return None
